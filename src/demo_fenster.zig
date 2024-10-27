004a9971d51122bd39fe117e1529bc773a83a15f
afaber999 <albert.faber@philips.com>
2024-10-27 21:34:19 +0100

    add readme

--- src/demo_fenster.zig
+++ src/demo_fenster.zig
@@ -3,9 +3,15 @@ const c = @cImport({
     @cInclude("fenster.h");
 });
 const Canvas = @import("Canvas.zig");
+const Dot3d = @import("demo_dot3d.zig");
+const Squish = @import("demo_squish.zig");
 const Triangle = @import("demo_triangle.zig");
 
-
+const Demo = union(enum) {
+    dot3d: Dot3d,
+    squish: Squish,
+    triangle: Triangle,
+};
 
 pub fn main() !void {
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
@@ -14,11 +20,39 @@ pub fn main() !void {
     const width = 800;
     const height = 600;
 
+    const args = try std.process.argsAlloc(allocator);
+    defer std.process.argsFree(allocator, args);
+
+    if (args.len < 2) {
+        std.debug.print("Usage: {s} <demo>\n", .{args[0]});
+        return;
+    }
+
     // nedded for ARGB to RGBA conversion
     var fenster_buffer = try allocator.alloc(u32, width * height);
+    var canvas: Canvas = undefined;
 
-    var demo = try Triangle.init(allocator, width, height);
-    defer demo.deinit();
+    var demo: Demo = undefined;
+
+    const demoName = args[1];
+    if (std.mem.eql(u8, demoName, "dot3d")) {
+        demo = Demo{ .dot3d = try Dot3d.init(allocator, width, height) };
+        canvas = demo.dot3d.canvas;
+    } else if (std.mem.eql(u8, demoName, "squish")) {
+        demo = Demo{ .squish = try Squish.init(allocator, width, height) };
+        canvas = demo.squish.canvas;
+    } else if (std.mem.eql(u8, demoName, "triangle")) {
+        demo = Demo{ .triangle = try Triangle.init(allocator, width, height) };
+        canvas = demo.triangle.canvas;
+    } else {
+        return error.Unreachable;
+    }
+
+    defer switch (demo) {
+        .dot3d => demo.dot3d.deinit(),
+        .squish => demo.squish.deinit(),
+        .triangle => demo.triangle.deinit(),
+    };
 
     var f = std.mem.zeroInit(c.fenster, .{
         .width = width,
@@ -30,16 +64,23 @@ pub fn main() !void {
     _ = c.fenster_open(&f);
     defer c.fenster_close(&f);
 
-    var t: u32 = 0;
     var now: i64 = c.fenster_time();
+    var prev: i64 = now;
 
     while (c.fenster_loop(&f) == 0) {
-        _ = demo.render(15.0);
+        const dt: f32 = @floatFromInt(now - prev);
+        prev = now;
+        
+        _ = switch (demo) {
+            .dot3d => demo.dot3d.render(dt),
+            .squish => demo.squish.render(dt),
+            .triangle => demo.triangle.render(dt),
+        };
 
         var idx: usize = 0;
-        for (0..demo.canvas.height) |y| {
-            for (0..demo.canvas.width) |x| {
-                const pixel = demo.canvas.pixel_value(x, y);
+        for (0..canvas.height) |y| {
+            for (0..canvas.width) |x| {
+                const pixel = canvas.pixel_value(x, y);
                 fenster_buffer[idx] = Canvas.from_rgba(Canvas.blue(pixel), Canvas.green(pixel), Canvas.red(pixel), 255);
                 idx += 1;
             }
@@ -49,11 +90,6 @@ pub fn main() !void {
         if (f.keys[27] != 0) {
             break;
         }
-        // // Render x^y^t pattern
-        // for (buf, 0..) |_, i| {
-        //     buf[i] = @as(u32, @intCast(i % 320)) ^ @as(u32, @intCast(i / 240)) ^ t;
-        // }
-        t +%= 1;
         // Keep ~60 FPS
         const diff: i64 = 1000 / 60 - (c.fenster_time() - now);
         if (diff > 0) {
---
+++ src/readme.md
@@ -0,0 +1,16 @@
+
+
+# run wasm samples
+start http server for zig-out\html\index.html
+
+
+# run native examples
+
+zig-out\bin\zigviz.exe demoName
+or use zig build run -- demoName
+
+demoName
+    squish
+    dot3d
+    triangle
+  
